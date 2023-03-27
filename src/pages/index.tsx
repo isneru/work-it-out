507795b3ff7f98751aeda3468a56afb3b241d3c2
isneru <diogoclanogueira@gmail.com>
2023-03-27 04:55:06 +0100

    Auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs"
 import { type NextPage } from "next"
 import Head from "next/head"
 
 const Home: NextPage = () => {
+  const { isSignedIn, user } = useUser()
   return (
     <>
       <Head>
@@ -11,6 +13,8 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center">
         <h1>Hello, world</h1>
+        {!!user && <SignOutButton>Sign Out</SignOutButton>}
+        {!user && <SignInButton>Sign In</SignInButton>}
       </main>
     </>
   )
